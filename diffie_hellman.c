3b3f365806bbabec6f58a8a199a3f657153b4e9e
cleon <cleon.form@gmail.com>
2021-12-11 16:11:56 +0100

    Incluida version python

--- diffie_hellman.c
+++ diffie_hellman.c
@@ -3,30 +3,25 @@
 #include <time.h>
 #include <math.h>
 
-int random_number();
-int mod_power();
+double random_number();
+double mod_power();
 
-double main(){
-    int p, r, x, X, Y, K;
-    p = 71;
-    r = 7;
-    x = 40;
-    /*Calculamos la clave pública del usuario
+double main(void){
+    double p, r, x, X, Y, K;
+    p = 761;
+    r = 6  ;
+    x = random_number();
+    /*Calculamos la clave pública del usuario*/
     X = fmod(powf(r, x), p);
-    printf("Send the number X (public key): %d to your partner.\n", (int) X);*/
+    printf("Send the number X (public key): %d to your partner.\n", (int) X);
     
-    int kk = mod_power(r, x);
-    printf("Resultado de la potencia %d\n", kk);
-    int kkk =  kk % p;
-    printf("El cálculo del resto es %d\n", kkk);
-
     /*Obtenemos la clave pública del usuario 2*/
     printf("Enter the Y number that your partner has sent you: "); 
-    scanf("%d", &Y);
-
-    /*Obtenemos la clave común para ambos usuarios
+    scanf("%f", &Y);
+    
+    /*Obtenemos la clave común para ambos usuarios*/
     K = fmod(powf(Y, x), p);
-    printf("\nLa llave común K es: %lf", K);*/
+    printf("La llave común K es: %lf", K);
     
     printf("\n");
     return 0;
@@ -34,22 +29,22 @@ double main(){
 
 /*Generador de números aleatorios; 
 limitado para evitar el desbordamiento de pila*/
-int random_number(){
-    int rand_num;
+double random_number(){
+    double rand_num;
     srand(time(0));
     rand_num = rand() % (9-0) + 0;
     /*TODO: Se muestra clave privada x por control. Eliminar antes de enviar*/
-    printf("El número aleatorio generado es: %d\n", rand_num);
+    printf("El número aleatorio generado es: %f\n", rand_num);
     return rand_num;
 }
 
-int mod_power(int numero, int exponente){
-    int numeroTemp;
-    int i;
+double mod_power(double numero, double exponente){
+    double numeroTemp;
+    double i;
     numeroTemp = 1;
     for (i = 1; i <= exponente; ++i){
         numeroTemp = numeroTemp * numero;
-        printf("El numero es %d\n", numeroTemp);
+        printf("El numero es %f\n", numeroTemp);
     }
     return numeroTemp;
 }
