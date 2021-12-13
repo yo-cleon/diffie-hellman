35a95e7ce8365a7b989c26fac9ea6c088083448d
Carlos <cleon.form@gmail.com>
2021-12-13 17:44:51 +0100

    Modificada cálculo de la potencia

--- diffie-hellmanB.py
+++ diffie-hellmanB.py
@@ -1,29 +1,35 @@
 import random
-import math
+#import math
 
-def random_number():
-    rand_num = random.randint(0,90)
-    print("El número aleatorio generado es " + str(rand_num))
+def random_number(p):
+    rand_num = random.randint(0,p-1)
+    #print("El número aleatorio generado es " + str(rand_num))
     return rand_num
 
+def calcula_potencia(r, x):
+    num = 0
+    for i in range(x+1):
+        num = r**i
+    return num
+
 def mod_exp(r, x, p):
-    exp = math.pow(r, x)
-    print("La potencia es " + str(exp))
+    #exp = math.pow(r, x)
+    exp = calcula_potencia(r, x)
+    #print("La potencia es " + str(exp))
     result = exp % p
     return result
 
 
-p = 23
-r = 5
-x = 3
+p = 761
+r = 6
+x = random_number(p)
 
 X = mod_exp(r, x, p)
 
-print("Envía el número X (clave pública) a tu compañero " + str(X))
+print("Envía el número Y (clave pública) a tu compañero " + str(X))
 
 
 Y = int(input())
-
 K = mod_exp(Y, x, p)
 
 print("El secreto es " + str(K))
