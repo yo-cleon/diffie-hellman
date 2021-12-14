eba31d5cff789956a2709fe71d38aa362a8792e1
yo-cleon <cleon.form@gmail.com>
2021-12-14 23:20:36 +0000

    Agrupados archivos obsoletos

--- old/diffie_hellman.c
+++ old/diffie_hellman.c
@@ -3,25 +3,25 @@
 #include <time.h>
 #include <math.h>
 
-double random_number();
-double mod_power();
+int random_number();
+int mod_power();
 
-double main(void){
-    double p, r, x, X, Y, K;
+int main(void){
+    int p, r, x, X, Y, K;
     p = 761;
     r = 6  ;
     x = random_number();
     /*Calculamos la clave pública del usuario*/
-    X = fmod(powf(r, x), p);
+    X = fmod(mod_power(r, x), p);
     printf("Send the number X (public key): %d to your partner.\n", (int) X);
     
     /*Obtenemos la clave pública del usuario 2*/
     printf("Enter the Y number that your partner has sent you: "); 
-    scanf("%f", &Y);
+    scanf("%d", &Y);
     
-    /*Obtenemos la clave común para ambos usuarios*/
+    /*Obtenemos la clave com�n para ambos usuarios*/
     K = fmod(powf(Y, x), p);
-    printf("La llave común K es: %lf", K);
+    printf("La llave com�n K es: %d", K);
     
     printf("\n");
     return 0;
@@ -29,22 +29,22 @@ double main(void){
 
 /*Generador de números aleatorios; 
 limitado para evitar el desbordamiento de pila*/
-double random_number(){
-    double rand_num;
+int random_number(){
+    int rand_num;
     srand(time(0));
-    rand_num = rand() % (9-0) + 0;
+    rand_num = rand() % (100-0) + 0;
     /*TODO: Se muestra clave privada x por control. Eliminar antes de enviar*/
-    printf("El número aleatorio generado es: %f\n", rand_num);
+    printf("El numero aleatorio generado es: %d\n", rand_num);
     return rand_num;
 }
 
-double mod_power(double numero, double exponente){
-    double numeroTemp;
-    double i;
+int mod_power(int numero, int exponente){
+    int numeroTemp;
+    int i;
     numeroTemp = 1;
     for (i = 1; i <= exponente; ++i){
         numeroTemp = numeroTemp * numero;
-        printf("El numero es %f\n", numeroTemp);
+        printf("El numero es %d\n", numeroTemp);
     }
     return numeroTemp;
 }
